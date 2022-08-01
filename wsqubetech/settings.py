62290fe7480419e9dcb983c151c22aa06dc3aced
Pradeep Rajpurohit <53520182+pradeeprajpurohit2999@users.noreply.github.com>
2022-08-01 15:23:14 +0530

    Update settings.py

--- wsqubetech/settings.py
+++ wsqubetech/settings.py
@@ -145,7 +145,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
