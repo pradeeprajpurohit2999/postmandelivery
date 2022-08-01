c394d118a3150f801538380ac9906ad2f7352209
pradeeprajpurohit2999 <pradeeprajpurohit2999@gmail.com>
2022-08-01 12:27:53 +0530

    side branch

--- wsqubetech/wsgi.py
+++ wsqubetech/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for wsqubetech project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wsqubetech.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wsqubetech.heroku_settings') 
+application = StaticFilesHandler(get_wsgi_application())
